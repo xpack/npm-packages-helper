54f1cf1451ab713702a525ff7a665aa2c9b0dec3
Liviu Ionescu <ilg@livius.net>
2026-06-14 13:59:33 +0300

    template cosmetics

--- templates/common/scripts/clang-format.sh
+++ templates/common/scripts/clang-format.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 # -----------------------------------------------------------------------------
 # DO NOT EDIT!
 # Automatically generated from npm-packages-helper/templates/*.
